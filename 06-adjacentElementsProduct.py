c2275be0c3bb86a346ec7a3206eeba6a4db6eedb
Lucas Alexander <lucasalexander13@hotmail.com>
2021-08-24 19:27:55 -0300

    Added challenge solution

--- 06-adjacentElementsProduct.py
+++ 06-adjacentElementsProduct.py
@@ -1,3 +1,17 @@
 # Given an array of integers, find the pair of adjacent elements 
 # that has the largest product and return that product.
 
+def adjacentElementsProduct(inputArray):
+    high_value = -1000
+    
+    for i in range(len(inputArray)):
+        if i == len(inputArray) - 1:
+            break
+        else:    
+            value_one = inputArray[i]
+            value_two = inputArray[i + 1]
+            product = value_one * value_two
+            
+            if product > high_value:
+                high_value = product
+    return high_value
