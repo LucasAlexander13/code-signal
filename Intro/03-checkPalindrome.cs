430754ccf50f5b695a872bd062e6ead16ee5c3f9
Lucas Alexander <lucasalexander13@hotmail.com>
2021-08-26 22:16:39 -0300

    Added solution in c#

--- Intro/03-checkPalindrome.cs
+++ Intro/03-checkPalindrome.cs
@@ -1,1 +1,19 @@
 // Given the string, check if it is a palindrome.
+
+string reverseString(string str) {  
+    if (str.Length <= 1) {
+        return str;
+    }
+    else {
+        return reverseString(str.Substring(1)) + str[0];
+    }
+}  
+
+bool checkPalindrome(string inputString) {
+    if (inputString == reverseString(inputString)){
+        return true;
+    }
+    else {
+        return false;
+    }
+}
